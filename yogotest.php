db4c64042c4796c6027bd952d67b89d9097f6381
wakhidulkhoiri <42726508+wakhidulkhoiri@users.noreply.github.com>
2018-11-15 10:58:09 +0700

    Rename test.php to yogotest.php

---
+++ yogotest.php
@@ -0,0 +1,48 @@
+<?php
+	// Created by: Fico Pangestu
+	// Not For Sale
+	function yogo($deviceCode, $deviceId, $sign, $tk, $token, $uuid, $jumlah, $wait){
+	    $x = 0; 
+	    while($x < $jumlah) {
+			
+	$body = ' deviceCode ='.$deviceCode.'& deviceId ='.$deviceId.'&sign='$sign.'&tk='.$tk.'&token='.$token.'&uuid='.$uuid.'';
+					
+	        $ch = curl_init();
+	        curl_setopt($ch, CURLOPT_URL,"https://api.yogopro.com/api/reward/watchVideo/$rand");
+	        curl_setopt($ch, CURLOPT_POST, 1);
+	        curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+	        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	        $server_output = curl_exec ($ch);
+	        curl_close ($ch);
+			echo $server_output."\n";
+	        sleep($wait);
+	        $x++;
+	        flush();
+	    }
+	}
+	print "
+	\033[32m##############################################################################\033[0m
+				    \033[32mTuyul coin YogoVideo\033[32m
+						
+	\033[32m##############################################################################\033[0m";
+	echo "\n\ndeviceCode: ";
+	$deviceCode = trim(fgets(STDIN));
+	echo "DeviceId: ";
+	$deviceId = trim(fgets(STDIN));
+	echo "Sign: ";	
+	$deviceId = trim(fgets(STDIN));
+	$sign = trim(fgets(STDIN));	
+	echo "tk: ";
+	echo "Token: ";
+	$token = trim(fgets(STDIN));
+	echo "UUID: ";
+	$uuid = trim(fgets(STDIN));
+	echo "Jumlah: ";
+	$jumlah = trim(fgets(STDIN));
+	echo "Jeda? 0-9999999999 (ex:60): ";
+	$wait = trim(fgets(STDIN));
+	$execute = yogo($deviceCode, $deviceId, $sign, $tk, $token, $uuid, $jumlah, $wait);
+	print $execute;
+	print "DONE\n";
+	?>
