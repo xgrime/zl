73fcfddff6913d2385bc45b0f84d064ed01be710
wakhidulkhoiri <42726508+wakhidulkhoiri@users.noreply.github.com>
2018-12-14 14:17:09 +0700

    Create cfg.php

---
+++ cfg.php
@@ -0,0 +1,10 @@
+<?php
+//ikuti format config di bawah "ticket=tiket_kamu&meid=meid_kamu",
+//jangan ada sepasi
+$config = [
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh4TRruCjsL_LipWxsJmkfXaqko7cdpy0q5dmkaDRmICHnd6vqp9os6WdzK2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=2810ae3922666e91",
+
+//"ticket=akun tuyul&meid=akun tuyul", hilangkan tanda // untuk menambahkan
+];
+
+?>
