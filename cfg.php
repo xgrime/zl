a46ba2086c41dc2865e1de655626c7651aac1649
xgrime <45916886+xgrime@users.noreply.github.com>
2018-12-16 23:54:21 +0700

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -2,7 +2,7 @@
 //ikuti format config di bawah "ticket=tiket_kamu&meid=meid_kamu",
 //jangan ada sepasi
 $config = [
-"ticket=sHp2r312otmCpnGns3mCqJhovNiBh3jPr7qja7OTitm8sIpofay2k4SmlJi_Y4Kbhn2szX53hM264JKks5N_y62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=aa12d572eba63cd9",
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh52YurqBasC1lNuwaH-gfYbFzY-mipi1dYljkY2zmH-KjNG70JGwtLWdzK2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=2810ae3922666e91",
 ];
 
 ?>
